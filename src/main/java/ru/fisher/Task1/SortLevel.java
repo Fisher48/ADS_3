e170eb5a3c2ffa3491921aeb19f24e9a8d613df2
Fisher48 <rybakov48@yandex.ru>
2025-12-02 09:40:16 +0300

    Added Task 1

---
+++ src/main/java/ru/fisher/Task1/SortLevel.java
@@ -0,0 +1,40 @@
+package ru.fisher.Task1;
+
+public class SortLevel {
+
+    public static void SelectionSortStep(int[] array, int i) {
+        if (i >= array.length) {
+            return;
+        }
+        int minInd = 0;
+        for (int j = 0; j < array.length - 1; j++) {
+            if (array[j] < array[j + 1]) {
+                minInd = j;
+            }
+        }
+        if (array[i] > array[minInd]) {
+            swapElements(array, i, minInd);
+        }
+    }
+
+    public static void swapElements(int[] array, int i, int minInd) {
+        int temp;
+        temp = array[minInd];
+        array[minInd] = array[i];
+        array[i] = temp;
+    }
+
+    public static boolean BubbleSortStep(int[] array) {
+        boolean isNotChanged = true;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                swapElements(array, i, i + 1);
+                isNotChanged = false;
+            }
+        }
+        return isNotChanged;
+    }
+
+}
+
+
---
+++ src/main/java/ru/fisher/Task1/SortLevelTest.java
@@ -0,0 +1,67 @@
+package ru.fisher.Task1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class SortLevelTest {
+
+    @Test
+    void testNotSortedArrayShouldChangeOneElement() {
+        int[] arr = new int[]{1, 7, 12, 0, 5};
+        int index = 2;
+        SortLevel.SelectionSortStep(arr, index);
+        assertArrayEquals(new int[]{1, 7, 0, 12, 5}, arr);
+    }
+
+    @Test
+    void testWithNotSortedArrayWithNegativeElementsAndPositiveShouldChangeOneElement() {
+        int[] arr = new int[]{1, -7, 12, 10, -5};
+        int index = 3;
+        SortLevel.SelectionSortStep(arr, index);
+        assertArrayEquals(new int[]{1, 10, 12, -7, -5}, arr);
+    }
+
+    @Test
+    void testSortedArrayShouldNotChangeOneElement() {
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        int index = 1;
+        SortLevel.SelectionSortStep(arr, index);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, arr);
+    }
+
+    @Test
+    void testSortedArrayWithOnlyNegativeElementsShouldNotChangeOneElement() {
+        int[] arr = new int[]{-12, -10, -8, -6, -2};
+        int index = 3;
+        SortLevel.SelectionSortStep(arr, index);
+        assertArrayEquals(new int[]{-12, -10, -8, -6, -2}, arr);
+    }
+
+    @Test
+    void testWithEmptyArrayShouldNotChangeAnyElement() {
+        int[] arr = new int[]{};
+        int index = 2;
+        SortLevel.SelectionSortStep(arr, index);
+        assertArrayEquals(new int[]{}, arr);
+    }
+
+    @Test
+    void testBubbleSortShouldChangeElement() {
+        int[] arr = new int[]{1, -7, 12, 10, -5};
+        assertFalse(SortLevel.BubbleSortStep(arr));
+    }
+
+    @Test
+    void testBubbleSortShouldNotChangeElement() {
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        assertTrue(SortLevel.BubbleSortStep(arr));
+    }
+
+    @Test
+    void testBubbleSortForEmptyArray() {
+        int[] arr = new int[]{};
+        assertTrue(SortLevel.BubbleSortStep(arr));
+    }
+}
